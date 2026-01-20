ee7ae3d54fe11403fba05468d7f7a96b2dca3e31
leonischu <nischal.201423@ncit.edu.np>
2026-01-20 15:07:17 +0545

    if/else

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,30 +9,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
- name = ""
- displayName = "";
- email = "";
-  getName(event:Event){
-    const name = ((event.target as HTMLInputElement).value);
-    this.name= name;
-  }
-  showName(){
-    this.displayName = this.name
-  }
-  setName(){
-    this.name = "Nischal"
 
-  }
-  getEmail(val:string){
-    console.log(val)
-    this.email = val;
-
-  }
-  setEmail(){
+  display=true;
   
-    this.email = "Default@test.com";
-
+  hide(){
+    this.display = false
   }
-
+ 
+  show(){
+    this.display = true
+  }
+  Toggle(){
+    this.display = !this.display
+  }
  
 }
