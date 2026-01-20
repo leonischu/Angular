8feea756af611cb20dafb7ee7de745d268deb497
leonischu <nischal.201423@ncit.edu.np>
2026-01-20 11:30:31 +0545

    Learn Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular_tut');
+  name = "Hello World"
+   age = 20
+   getName(a:number,b:number){
+    return a+b;
+   }
 }
