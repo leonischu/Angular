92f22205e0a02218c330421ebd9ed6add5222328
leonischu <nischal.201423@ncit.edu.np>
2026-01-20 16:19:34 +0545

    Data Type with signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 
@@ -9,25 +9,40 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-count = signal(10)
-x = 20;
-constructor(){
-  effect(()=>{
-    console.log(this.count());
-  })
 
+// data = signal<number | string>(10);
+data: WritableSignal<number | string> = signal(10);
+// count:Signal<number> = computed(()=>200 ); // define garesi esko value change garna mildaina 
+updateSignal(){
+  this.data.set("hello")
 }
 
-updateValue(val:string){
 
-  if(val=='inc'){
-  this.count.set(this.count()+1);
-  // this.x= this.x+1;
-  }
-else {
-this.count.set(this.count()-1)
-}
 
-}
+
+
+
+
+// count = signal(10)
+// x = 20;
+// constructor(){
+//   effect(()=>{
+//     console.log(this.count());
+//   })
+
+// }
+
+// updateValue(val:string){
+
+//   if(val=='inc'){
+//   this.count.set(this.count()+1);
+//   // this.x= this.x+1;
+//   }
+// else {
+// this.count.set(this.count()-1)
+// }
+
+
+// }
  
 }
