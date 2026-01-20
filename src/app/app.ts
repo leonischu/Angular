3e674ea31f9172813b2a727593f44e5cee7ef228
leonischu <nischal.201423@ncit.edu.np>
2026-01-20 11:41:01 +0545

    Function call

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular_tut');
-  name = "Hello World"
-   age = 20
-   getName(a:number,b:number){
-    return a+b;
-   }
+  handleClickEvent(){
+
+    alert("Function called")
+    console.log("Function called")
+    this.otherFunction()
+  }
+  
+  otherFunction(){
+    console.log("other function");
+  }
+  
 }
