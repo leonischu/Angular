a0de7696daa3c399ae68a2725bcfa207acf9125b
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 13:58:39 +0545

    404 Page Not Found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { About } from './about/about'
 import { Contact } from './contact/contact';
 import { Login } from './login/login';
 import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
    {path:'about',component:About},
    {path:'login',component:Login},
    {path:'contact',component:Contact},
-   {path:'',component:Home}
+   {path:'',component:Home},
+   {path:'**',component:PageNotFound }
 
 ];
