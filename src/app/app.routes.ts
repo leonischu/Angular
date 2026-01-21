2086265e2547aa22ca096de47b6a5ece46adbe3f
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 14:29:27 +0545

    pass data from one page to other

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Contact } from './contact/contact';
 import { Login } from './login/login';
 import { Home } from './home/home';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
    {path:'about',component:About},
    {path:'login',component:Login},
    {path:'contact',component:Contact},
    {path:'',component:Home},
+   {path:'profile',component:Profile,data:{name:'nis'}},
    {path:'**',component:PageNotFound }
 
 ];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+
+
+  constructor(private router:Router){
+    
+  }
+
+  // goToProfile(name:string){
+  //   this.router.navigate(['profile'],{queryParams:{name}})
+
+  // }
 
 }
