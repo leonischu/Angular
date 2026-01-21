130f4d5ee938f0441aeb4bc12ae2deb58b7291ac
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 12:21:56 +0545

    Basic Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about'
+import { Contact } from './contact/contact';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {path:'about',component:About},
+   {path:'login',component:Login},
+   {path:'contact',component:Contact}
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,20 @@
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule,NgIf,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
-color = "black"
 
- changeColor(color:string){
-  this.color = color
- }
 
 }
 
-
-
-  // show = true
-  // students = ['ram','hari','sam'];
-  // login = false;
 
 
  
