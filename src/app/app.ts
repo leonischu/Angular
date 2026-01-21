9b8305b0402ba6c80a250af9eb813a7effa1e298
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 17:01:40 +0545

    Templete Driven Forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,22 @@
 import { NgIf } from '@angular/common';
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ReactiveFormsModule,NgIf],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
-  profileForm =new FormGroup({
-    name: new FormControl('',[Validators.required]),
-    password: new FormControl('',[Validators.required,Validators.minLength(5)]),
-    email: new FormControl('',[Validators.required,Validators.maxLength(50),Validators.pattern('^[a-z0-9._%+-]')]),
-
-  })
-  onSubmit(){
-    console.log(this.profileForm.value);
-  }
-  get name(){
-    return this.profileForm.get('name')
-  }
-  get email(){
-    return this.profileForm.get('email')
-  }
-  get password(){
-    return this.profileForm.get('password')
-  }
+userDetails:any;
+addDetails(val:NgForm){
+  console.log(val)
+  this.userDetails = val;
+}
 
 
 }
