3a9677f5643b6c95e83ab6b3baba5aa813a55faf
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 13:46:48 +0545

    Header with Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { About } from './about/about'
 import { Contact } from './contact/contact';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 export const routes: Routes = [
    {path:'about',component:About},
    {path:'login',component:Login},
-   {path:'contact',component:Contact}
+   {path:'contact',component:Contact},
+   {path:'',component:Home}
 
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
