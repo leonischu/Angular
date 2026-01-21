7821bcea72fda8f09046b7bfa01cc5d2b2eea906
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 15:56:45 +0545

    Basic reactive forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,31 @@
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
+  name = new FormControl();
+  password = new FormControl();
+
+  displayValue(){
+    console.log(this.name.value,this.password.value);
+    
+  }
+  setValues(){
+
+    this.name.setValue("peter");
+    this.password.setValue('213')
+
+
+  }
 
 
 }
