dc866f8f6d7ff57f851354e12260f74c60400577
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 16:47:58 +0545

    Form Validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,35 @@
+import { NgIf } from '@angular/common';
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ReactiveFormsModule],
+  imports: [RouterOutlet,ReactiveFormsModule,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
   profileForm =new FormGroup({
-    name: new FormControl(),
-    password: new FormControl(),
-    email: new FormControl(),
+    name: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required,Validators.minLength(5)]),
+    email: new FormControl('',[Validators.required,Validators.maxLength(50),Validators.pattern('^[a-z0-9._%+-]')]),
 
   })
   onSubmit(){
     console.log(this.profileForm.value);
+  }
+  get name(){
+    return this.profileForm.get('name')
+  }
+  get email(){
+    return this.profileForm.get('email')
+  }
+  get password(){
+    return this.profileForm.get('password')
   }
 
 
