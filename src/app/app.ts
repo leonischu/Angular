3c984c147652f3dc17f0a791f796ba8f0f36ea84
leonischu <nischal.201423@ncit.edu.np>
2026-01-21 11:56:57 +0545

    *ng Directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,4 @@
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -5,23 +6,25 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,NgIf,NgFor,NgSwitch,NgSwitchCase,NgSwitchDefault],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  bgColor = "green"
-  fontSize = "100px"
-  headingSizeBig = "40px"
-  headingSizeSmall = "10px"
 
-  zoom = false
-  update(){
-    this.zoom = !this.zoom
-  }
+color = "black"
+
+ changeColor(color:string){
+  this.color = color
+ }
 
 }
 
+
+
+  // show = true
+  // students = ['ram','hari','sam'];
+  // login = false;
 
 
  
