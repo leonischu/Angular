ea7f69d05d58397048157c60ea6b4b8dd517cc15
leonischu <nischal.201423@ncit.edu.np>
2026-01-22 10:12:49 +0545

    Passing data from parent to child

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,20 +2,21 @@ import { NgIf } from '@angular/common';
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { User } from './user/user';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule,NgIf],
+  imports: [User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-userDetails:any;
-addDetails(val:NgForm){
-  console.log(val)
-  this.userDetails = val;
+
+userName = "Bruce"
+onUserChange(user:string){
+  this.userName = user;
 }
 
 
